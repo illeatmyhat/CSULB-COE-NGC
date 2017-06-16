219702f9b43274e929e3a8eb16d1129278e4cba3
Unknown <atonysanchez1@gmail.com>
2017-06-16 12:41:26 -0700

    Image Constructor Constraints Deviations
    Each constraints has its own deviations and the positive images now have randomiation for negative constraints in positive canvas.

--- Training/Master.py
+++ Training/Master.py
@@ -1,6 +1,6 @@
 import Training
-#Deviations for the postive, Size, R,G,B,Theta
-deviations = [.60,.20,.90,.90,.10]
-list = [("square",5,225,6,7,32,False),("square",2,5,226,7,32,False),("square",7,255,6,227,32,False),("circle",8,200,200,200,32,True)]
+#Each constraint has paramter (shape, size, R, G, B, theta, bool, sizeDev,RDev,GDev,BDev,thetaDev)
+list = [("square",5,225,6,7,32,True,.10,.20,.90,.90,.10),("square",4,5,226,7,32,False,.1,.0,.0,.0,.0),
+        ("square",7,255,6,227,32,False,.9,.0,.0,.0,.0),("circle",8,200,0,0,32,True,.50,.9,.4,.2,.0)]
 imageCount = 14
-Training.main(list,256, imageCount,deviations,"Training/")
+Training.main(list,256, imageCount,"Training/")
--- Training/Training.py
+++ Training/Training.py
@@ -8,16 +8,17 @@ def generateLocations(imageSize, imageCount):
     return [divmod(x, imageSize) for x in random.sample(range(imageSize * imageSize), imageCount)]
 
 #Saves the pictures to positves folder
-def generatePositiveImages(listOfParameters, listOfShapes, positiveCanvas, deviations, url):
+def generatePositiveImages(listOfParameters, listOfShapes, positiveCanvas, url):
     for locations in listOfParameters:
         img = ImageDraw.Draw(positiveCanvas)
         for index, shapeProp in enumerate(listOfShapes):
             loc = locations[index]
             if(shapeProp[6] == True):
-                size = random.randint(shapeProp[1] - int(shapeProp[1] * deviations[0]),int(shapeProp[1] * deviations[0]) + shapeProp[1] )
-                R = random.randint(shapeProp[2] - int(shapeProp[2] * deviations[1]),int(shapeProp[2] * deviations[1]) + shapeProp[2])
-                G = random.randint(shapeProp[3] - int(shapeProp[3] * deviations[2]),int(shapeProp[3] * deviations[2]) + shapeProp[3])
-                B = random.randint(shapeProp[4] - int(shapeProp[4] * deviations[3]),int(shapeProp[4] * deviations[3]) + shapeProp[4])
+                size = random.randint(shapeProp[1] - int(shapeProp[1] * shapeProp[7]),int(shapeProp[1] * shapeProp[7]) + shapeProp[1])
+                R = random.randint(shapeProp[2] - int(shapeProp[2] * shapeProp[8]),int(shapeProp[2] *  shapeProp[8]) + shapeProp[2])
+                G = random.randint(shapeProp[3] - int(shapeProp[3] * shapeProp[9]),int(shapeProp[3] *  shapeProp[9]) + shapeProp[3])
+                B = random.randint(shapeProp[4] - int(shapeProp[4] * shapeProp[10]),int(shapeProp[4] *  shapeProp[10]) + shapeProp[4])
+                theta = random.randint(shapeProp[5] - int(shapeProp[5] * shapeProp[11]), int(shapeProp[5] * shapeProp[11]) + shapeProp[5])
                 # Setting shapeProp(shape Properties) equal to the index in list of shapes, which will be a tuple of (shape, size, R-value, G-Value, B-Value, Theta)
                 # Therefore properties are availible by using shapeProp indices
                 shapeProp = listOfShapes[index]
@@ -30,19 +31,24 @@ def generatePositiveImages(listOfParameters, listOfShapes, positiveCanvas, devia
                     x = (loc[0] - size, loc[1] - size)
                     y = (loc[0] + size, loc[1] + size)
                     img.ellipse((x, y), fill=(R,G,B),outline=(R,G,B))
-            else:
-                #Setting shapeProp(shape Properties) equal to the index in list of shapes, which will be a tuple of (shape, size, R-value, G-Value, B-Value, Theta)
-                #Therefore properties are availible by using shapeProp indices
+            elif(random.randint(1,10) < 7):
+                size = random.randint(shapeProp[1] - int(shapeProp[1] * shapeProp[7]),int(shapeProp[1] * shapeProp[7]) + shapeProp[1])
+                R = random.randint(shapeProp[2] - int(shapeProp[2] * shapeProp[8]), int(shapeProp[2] * shapeProp[8]) + shapeProp[2])
+                G = random.randint(shapeProp[3] - int(shapeProp[3] * shapeProp[9]),int(shapeProp[3] * shapeProp[9]) + shapeProp[3])
+                B = random.randint(shapeProp[4] - int(shapeProp[4] * shapeProp[10]), int(shapeProp[4] * shapeProp[10]) + shapeProp[4])
+                theta = random.randint(shapeProp[5] - int(shapeProp[5] * shapeProp[11]),int(shapeProp[5] * shapeProp[11]) + shapeProp[5])
+                # Setting shapeProp(shape Properties) equal to the index in list of shapes, which will be a tuple of (shape, size, R-value, G-Value, B-Value, Theta)
+                # Therefore properties are availible by using shapeProp indices
                 shapeProp = listOfShapes[index]
-                #Cases depending on what shape property the shape holds, square, circle,etc...
-                if(shapeProp[0] == "square"):
-                    x = (loc[0] - shapeProp[1] , loc[1] - shapeProp[1])
-                    y = (loc[0] + shapeProp[1], loc[1] + shapeProp[1])
-                    img.rectangle((x, y), fill= (shapeProp[2],shapeProp[3],shapeProp[4]), outline=(shapeProp[2],shapeProp[3],shapeProp[4]))
-                if(shapeProp[0] == "circle"):
-                    x = (loc[0] - shapeProp[1], loc[1] - shapeProp[1])
-                    y = (loc[0] + shapeProp[1], loc[1] + shapeProp[1])
-                    img.ellipse((x, y), fill=(shapeProp[2], shapeProp[3], shapeProp[4]),outline=(shapeProp[2], shapeProp[3], shapeProp[4]))
+                # Cases depending on what shape property the shape holds, square, circle,etc...
+                if (shapeProp[0] == "square"):
+                    x = (loc[0] - size, loc[1] - size)
+                    y = (loc[0] + size, loc[1] + size)
+                    img.rectangle((x, y), fill=(R, G, B), outline=(R, G, B))
+                if (shapeProp[0] == "circle"):
+                    x = (loc[0] - size, loc[1] - size)
+                    y = (loc[0] + size, loc[1] + size)
+                    img.ellipse((x, y), fill=(R, G, B), outline=(R, G, B))
         positiveCanvas.save(url + "Positive/{}_{},{}_{}.png".format(loc[0], loc[1], loc[0], loc[1]), "PNG")
         positiveCanvas = canvas.copy()
 
@@ -53,23 +59,28 @@ def generateNegativeImages(listOfParameters, listOfShapes, negativeCanvas, url):
         for index, shapeProp in enumerate(listOfShapes):
             loc = locations[index]
             if(shapeProp[6] != True):
-                #Setting shapeProp(shape Properties) equal to the index in list of shapes, which will be a tuple of (shape, size, R-value, G-Value, B-Value, Theta)
-                #Therefore properties are availible by using shapeProp indices
+                size = random.randint(shapeProp[1] - int(shapeProp[1] * shapeProp[7]),int(shapeProp[1] * shapeProp[7]) + shapeProp[1])
+                R = random.randint(shapeProp[2] - int(shapeProp[2] * shapeProp[8]),int(shapeProp[2] * shapeProp[8]) + shapeProp[2])
+                G = random.randint(shapeProp[3] - int(shapeProp[3] * shapeProp[9]),int(shapeProp[3] * shapeProp[9]) + shapeProp[3])
+                B = random.randint(shapeProp[4] - int(shapeProp[4] * shapeProp[10]),int(shapeProp[4] * shapeProp[10]) + shapeProp[4])
+                theta = random.randint(shapeProp[5] - int(shapeProp[5] * shapeProp[11]),int(shapeProp[5] * shapeProp[11]) + shapeProp[5])
+                # Setting shapeProp(shape Properties) equal to the index in list of shapes, which will be a tuple of (shape, size, R-value, G-Value, B-Value, Theta)
+                # Therefore properties are availible by using shapeProp indices
                 shapeProp = listOfShapes[index]
-                #Cases depending on what shape property the shape holds, square, circle,etc...
-                if(shapeProp[0] == "square"):
-                    x = (loc[0] - shapeProp[1] , loc[1] - shapeProp[1])
-                    y = (loc[0] + shapeProp[1], loc[1] + shapeProp[1])
-                    img.rectangle((x, y), fill= (shapeProp[2],shapeProp[3],shapeProp[4]), outline=(shapeProp[2],shapeProp[3],shapeProp[4]))
-                if(shapeProp[0] == "circle"):
-                    x = (loc[0] - shapeProp[1], loc[1] - shapeProp[1])
-                    y = (loc[0] + shapeProp[1], loc[1] + shapeProp[1])
-                    img.ellipse((x, y), fill=(shapeProp[2], shapeProp[3], shapeProp[4]),outline=(shapeProp[2], shapeProp[3], shapeProp[4]))
+                # Cases depending on what shape property the shape holds, square, circle,etc...
+                if (shapeProp[0] == "square"):
+                    x = (loc[0] - size, loc[1] - size)
+                    y = (loc[0] + size, loc[1] + size)
+                    img.rectangle((x, y), fill=(R, G, B), outline=(R, G, B))
+                if (shapeProp[0] == "circle"):
+                    x = (loc[0] - size, loc[1] - size)
+                    y = (loc[0] + size, loc[1] + size)
+                    img.ellipse((x, y), fill=(R, G, B), outline=(R, G, B))
         negativeCanvas.save(url + "Negative/{}_{},{}_{}.png".format(loc[0], loc[1], loc[0], loc[1]), "PNG")
         negativeCanvas = canvas.copy()
 
 
-def main(listOfShapes, imageSize, imageCount,deviations,url):
+def main(listOfShapes, imageSize, imageCount,url):
     #Empty list that will be filled with the locations of the shapes, a list of seperate list for the locations, a set for each pair of shapes
     listOfParameters = []
     for x in range(imageCount):
@@ -78,8 +89,8 @@ def main(listOfShapes, imageSize, imageCount,deviations,url):
     positiveCanvas = canvas.copy()
     negativeCanvas = canvas.copy()
     #Saving images for positive's
-    generatePositiveImages(listOfParameters, listOfShapes, positiveCanvas,deviations,url)
+    generatePositiveImages(listOfParameters, listOfShapes, positiveCanvas,url)
     generateNegativeImages(listOfParameters, listOfShapes, negativeCanvas,url)
 
 if __name__ == "__main__":
-    main(listOfShapes =vars(), imageSize = vars(), imageCount = vars())
+    main(listOfShapes =vars(), imageSize = vars(), imageCount = vars(),url= vars())

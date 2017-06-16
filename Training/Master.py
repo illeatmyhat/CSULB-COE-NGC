dcdb2a8a861109530eed22ea695fdafbf9ce44b6
Unknown <atonysanchez1@gmail.com>
2017-06-16 15:35:24 -0700

    Antialiasing for Shapes
    Uses super sampling for rough edges on the shapes with rotations and circles. Takes account for theta along with deviation for it.

--- Training/Master.py
+++ Training/Master.py
@@ -1,14 +1,6 @@
 import Training
-<<<<<<< HEAD
 #Each constraint has paramter (shape, size, R, G, B, theta, bool, sizeDev,RDev,GDev,BDev,thetaDev)
-list = [("square",5,225,6,7,32,True,.10,.20,.90,.90,.10),("square",4,5,226,7,32,False,.1,.0,.0,.0,.0),
-        ("square",7,255,6,227,32,False,.9,.0,.0,.0,.0),("circle",8,200,0,0,32,True,.50,.9,.4,.2,.0)]
+list = [("square",200,225,6,7,0,True,.10,.10,.10,.10,.7),("square",4,5,226,7,0,False,.1,.0,.0,.0,.0),
+        ("square",7,255,6,227,0,False,.1,.0,.0,.0,.0),("circle",8,200,0,0,0,False,.10,.9,.4,.2,.0)]
 imageCount = 14
-Training.main(list,256, imageCount,"Training/")
-=======
-#Deviations for the postive, Size, R,G,B,Theta
-deviations = [.60,.20,.90,.90,.10]
-list = [("square",5,225,6,7,32,False),("square",2,5,226,7,32,False),("square",7,255,6,227,32,False),("circle",8,200,200,200,32,True)]
-imageCount = 14
-Training.main(list,256, imageCount,deviations,"Training/")
->>>>>>> master
+Training.main(list, imageCount,"Training/")
--- Training/Training.py
+++ Training/Training.py
@@ -1,9 +1,23 @@
 from PIL import Image, ImageDraw
-import PIL
+import math
 import random
 
-canvas = Image.new("RGB", (256, 256), "white")
+canvas = Image.new("RGB", (512, 512), "white")
 #Generates the locations of all the shapes, returns a list
+#finds the straight-line distance between two points
+def distance(ax, ay, bx, by):
+    return math.sqrt((by - ay)**2 + (bx - ax)**2)
+
+#rotates point `A` about point `B` by `angle` radians clockwise.
+def rotated_about(ax, ay, bx, by, angle):
+    radius = distance(ax,ay,bx,by)
+    angle += math.atan2(ay-by, ax-bx)
+    return (
+        round(bx + radius * math.cos(angle)),
+        round(by + radius * math.sin(angle))
+    )
+
+
 def generateLocations(imageSize, imageCount):
     return [divmod(x, imageSize) for x in random.sample(range(imageSize * imageSize), imageCount)]
 
@@ -13,42 +27,54 @@ def generatePositiveImages(listOfParameters, listOfShapes, positiveCanvas, url):
         img = ImageDraw.Draw(positiveCanvas)
         for index, shapeProp in enumerate(listOfShapes):
             loc = locations[index]
+            size = random.randint((2 * shapeProp[1]) - int((2 * shapeProp[1]) * shapeProp[7]), int((2 * shapeProp[1]) * shapeProp[7]) + (2 * shapeProp[1]))
+            R = random.randint(shapeProp[2] - int(shapeProp[2] * shapeProp[8]),int(shapeProp[2] * shapeProp[8]) + shapeProp[2])
+            G = random.randint(shapeProp[3] - int(shapeProp[3] * shapeProp[9]),int(shapeProp[3] * shapeProp[9]) + shapeProp[3])
+            B = random.randint(shapeProp[4] - int(shapeProp[4] * shapeProp[10]),int(shapeProp[4] * shapeProp[10]) + shapeProp[4])
+            theta = random.randint(shapeProp[5] - int(shapeProp[5] * shapeProp[11]),int(shapeProp[5] * shapeProp[11]) + shapeProp[5])
             if(shapeProp[6] == True):
-                size = random.randint(shapeProp[1] - int(shapeProp[1] * shapeProp[7]),int(shapeProp[1] * shapeProp[7]) + shapeProp[1])
-                R = random.randint(shapeProp[2] - int(shapeProp[2] * shapeProp[8]),int(shapeProp[2] *  shapeProp[8]) + shapeProp[2])
-                G = random.randint(shapeProp[3] - int(shapeProp[3] * shapeProp[9]),int(shapeProp[3] *  shapeProp[9]) + shapeProp[3])
-                B = random.randint(shapeProp[4] - int(shapeProp[4] * shapeProp[10]),int(shapeProp[4] *  shapeProp[10]) + shapeProp[4])
-                theta = random.randint(shapeProp[5] - int(shapeProp[5] * shapeProp[11]), int(shapeProp[5] * shapeProp[11]) + shapeProp[5])
                 # Setting shapeProp(shape Properties) equal to the index in list of shapes, which will be a tuple of (shape, size, R-value, G-Value, B-Value, Theta)
                 # Therefore properties are availible by using shapeProp indices
                 shapeProp = listOfShapes[index]
                 # Cases depending on what shape property the shape holds, square, circle,etc...
                 if (shapeProp[0] == "square"):
-                    x = (loc[0] - size, loc[1] - size)
-                    y = (loc[0] + size, loc[1] + size)
-                    img.rectangle((x, y), fill=(R, G, B),outline=(R,G,B))
+                    square_center = (loc[0] + (size / 2), loc[1] + (size / 2))
+                    square_vertices = (
+                        (square_center[0] + size / 2, square_center[1] + size / 2),
+                        (square_center[0] + size / 2, square_center[1] - size / 2),
+                        (square_center[0] - size / 2, square_center[1] - size / 2),
+                        (square_center[0] - size / 2, square_center[1] + size / 2)
+                    )
+                    square_vertices = [rotated_about(x, y, square_center[0], square_center[1], math.radians(theta))
+                                       for x, y in
+                                       square_vertices]
+                    img.polygon(square_vertices, fill=(R, G, B),outline=(R,G,B))
                 if (shapeProp[0] == "circle"):
                     x = (loc[0] - size, loc[1] - size)
                     y = (loc[0] + size, loc[1] + size)
                     img.ellipse((x, y), fill=(R,G,B),outline=(R,G,B))
             elif(random.randint(1,10) < 7):
-                size = random.randint(shapeProp[1] - int(shapeProp[1] * shapeProp[7]),int(shapeProp[1] * shapeProp[7]) + shapeProp[1])
-                R = random.randint(shapeProp[2] - int(shapeProp[2] * shapeProp[8]), int(shapeProp[2] * shapeProp[8]) + shapeProp[2])
-                G = random.randint(shapeProp[3] - int(shapeProp[3] * shapeProp[9]),int(shapeProp[3] * shapeProp[9]) + shapeProp[3])
-                B = random.randint(shapeProp[4] - int(shapeProp[4] * shapeProp[10]), int(shapeProp[4] * shapeProp[10]) + shapeProp[4])
-                theta = random.randint(shapeProp[5] - int(shapeProp[5] * shapeProp[11]),int(shapeProp[5] * shapeProp[11]) + shapeProp[5])
                 # Setting shapeProp(shape Properties) equal to the index in list of shapes, which will be a tuple of (shape, size, R-value, G-Value, B-Value, Theta)
                 # Therefore properties are availible by using shapeProp indices
                 shapeProp = listOfShapes[index]
                 # Cases depending on what shape property the shape holds, square, circle,etc...
                 if (shapeProp[0] == "square"):
-                    x = (loc[0] - size, loc[1] - size)
-                    y = (loc[0] + size, loc[1] + size)
-                    img.rectangle((x, y), fill=(R, G, B), outline=(R, G, B))
+                    square_center = (loc[0] + (size / 2), loc[1] + (size / 2))
+                    square_vertices = (
+                        (square_center[0] + size / 2, square_center[1] + size / 2),
+                        (square_center[0] + size / 2, square_center[1] - size / 2),
+                        (square_center[0] - size / 2, square_center[1] - size / 2),
+                        (square_center[0] - size / 2, square_center[1] + size / 2)
+                    )
+                    square_vertices = [rotated_about(x, y, square_center[0], square_center[1], math.radians(theta))
+                                       for x, y in
+                                       square_vertices]
+                    img.polygon(square_vertices, fill=(R, G, B), outline=(R, G, B))
                 if (shapeProp[0] == "circle"):
                     x = (loc[0] - size, loc[1] - size)
                     y = (loc[0] + size, loc[1] + size)
                     img.ellipse((x, y), fill=(R, G, B), outline=(R, G, B))
+        positiveCanvas.thumbnail((256, 256), Image.ANTIALIAS)
         positiveCanvas.save(url + "Positive/{}_{},{}_{}.png".format(loc[0], loc[1], loc[0], loc[1]), "PNG")
         positiveCanvas = canvas.copy()
 
@@ -59,7 +85,7 @@ def generateNegativeImages(listOfParameters, listOfShapes, negativeCanvas, url):
         for index, shapeProp in enumerate(listOfShapes):
             loc = locations[index]
             if(shapeProp[6] != True):
-                size = random.randint(shapeProp[1] - int(shapeProp[1] * shapeProp[7]),int(shapeProp[1] * shapeProp[7]) + shapeProp[1])
+                size = random.randint((2 * shapeProp[1]) - int((2 * shapeProp[1]) * shapeProp[7]),int((2 * shapeProp[1]) * shapeProp[7]) + (2 * shapeProp[1]))
                 R = random.randint(shapeProp[2] - int(shapeProp[2] * shapeProp[8]),int(shapeProp[2] * shapeProp[8]) + shapeProp[2])
                 G = random.randint(shapeProp[3] - int(shapeProp[3] * shapeProp[9]),int(shapeProp[3] * shapeProp[9]) + shapeProp[3])
                 B = random.randint(shapeProp[4] - int(shapeProp[4] * shapeProp[10]),int(shapeProp[4] * shapeProp[10]) + shapeProp[4])
@@ -69,22 +95,32 @@ def generateNegativeImages(listOfParameters, listOfShapes, negativeCanvas, url):
                 shapeProp = listOfShapes[index]
                 # Cases depending on what shape property the shape holds, square, circle,etc...
                 if (shapeProp[0] == "square"):
-                    x = (loc[0] - size, loc[1] - size)
-                    y = (loc[0] + size, loc[1] + size)
-                    img.rectangle((x, y), fill=(R, G, B), outline=(R, G, B))
+                    square_center = (loc[0] + (size / 2), loc[1] + (size / 2))
+                    square_vertices = (
+                        (square_center[0] + size / 2, square_center[1] + size / 2),
+                        (square_center[0] + size / 2, square_center[1] - size / 2),
+                        (square_center[0] - size / 2, square_center[1] - size / 2),
+                        (square_center[0] - size / 2, square_center[1] + size / 2)
+                    )
+                    square_vertices = [rotated_about(x, y, square_center[0], square_center[1], math.radians(theta))
+                                       for x, y in
+                                       square_vertices]
+                    img.polygon(square_vertices, fill=(R, G, B), outline=(R, G, B))
                 if (shapeProp[0] == "circle"):
                     x = (loc[0] - size, loc[1] - size)
                     y = (loc[0] + size, loc[1] + size)
                     img.ellipse((x, y), fill=(R, G, B), outline=(R, G, B))
+        negativeCanvas.thumbnail((256,256), Image.ANTIALIAS)
         negativeCanvas.save(url + "Negative/{}_{},{}_{}.png".format(loc[0], loc[1], loc[0], loc[1]), "PNG")
         negativeCanvas = canvas.copy()
 
 
-def main(listOfShapes, imageSize, imageCount,url):
+def main(listOfShapes, imageCount,url):
     #Empty list that will be filled with the locations of the shapes, a list of seperate list for the locations, a set for each pair of shapes
+    imageSize = 512
     listOfParameters = []
     for x in range(imageCount):
-        locationsList = generateLocations(imageSize,len(listOfShapes))
+        locationsList = generateLocations(imageSize - 10,len(listOfShapes))
         listOfParameters.append(locationsList)
     positiveCanvas = canvas.copy()
     negativeCanvas = canvas.copy()
@@ -93,4 +129,4 @@ def main(listOfShapes, imageSize, imageCount,url):
     generateNegativeImages(listOfParameters, listOfShapes, negativeCanvas,url)
 
 if __name__ == "__main__":
-    main(listOfShapes =vars(), imageSize = vars(), imageCount = vars(),url= vars())
+    main(listOfShapes =vars(), imageCount = vars(),url= vars())
